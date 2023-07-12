4477c4e195ad36395b9ff46e8e72a5b006f298aa
Leonardll <lonardlator@yahoo.fr>
2023-07-12 08:00:37 +0200

    🚧 customlimit order implementation done - start of OCO orders

--- app/api/binance/customOrder/route.ts
+++ app/api/binance/customOrder/route.ts
@@ -1,14 +1,14 @@
 // // export const dynamicParams = true;
 // export const runtime = 'nodejs'
 
-import { withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 import { NextResponse } from 'next/server';
 import crypto from 'crypto';
 
 const apiKey = process.env.BINANCE_API_KEY;
 const apiSecret = process.env.BINANCE_SECRET_KEY;
 
-async function fetchWithTimeout(resource, options, timeout = 1000) {
+async function fetchWithTimeout(resource:string, options:object, timeout = 1000) {
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), timeout);
 
@@ -28,19 +28,44 @@ export  const POST = async (req:any) => {
   let timeData = await timeRes.json();
   const end = Date.now();
   const latency = end - start;
- const body = await req.json();
+ 
+  const body = await req.json();
 
- if (!body || !body.symbol || !body.side || !body.quantity || !body.price || !body.type) {
-   throw new Error('Invalid request body!');
- }
- const { symbol, side,type, quantity, price } = body;
+ if (!body || !body.symbol || !body.side || !body.quantity || !body.price || !body.type || !body.icebergQty) {
+    throw new Error('Invalid request body!');
+  }
+ 
+ const { symbol, side,type, quantity, price, icebergQty, stopPrice  } = body;
+ // Create the base query string
+let queryString = `symbol=${symbol}&side=${side}&type=${type}&quantity=${quantity}`;
+
+// Add price if order is not MARKET type
+if (type !== 'MARKET') {
+  queryString += `&price=${price}`;
+}
+
+// Add icebergQty if it's not undefined
+if (icebergQty !== undefined) {
+  queryString += `&icebergQty=${icebergQty}`;
+}
+
+// Add stopPrice if it's not undefined
+if (stopPrice !== undefined) {
+  queryString += `&stopPrice=${stopPrice}`;
+}
 
 
-    // Create signature
-  let timestamp = Date.now()
-  const recvWindow = '50000';
-  const timeInForce = 'GTC';
-  const queryString = `symbol=${symbol}&side=${side}&timeInForce=${timeInForce}&type=${type}&quantity=${quantity}&price=${price}&recvWindow=${recvWindow}&timestamp=${timestamp}`;
+
+// Logic to apply time in foroce only for limit orders
+  const timeInForce = type !== 'MARKET' ? 'GTC' : null;
+  // if (icebergQty) {
+  //   queryString += `&icebergQty=${icebergQty}`;
+  // }
+  // Create signature
+let timestamp = Date.now()
+const recvWindow = '50000';
+// Add timestamp and recvWindow
+queryString += `&recvWindow=${recvWindow}&timestamp=${timestamp}`;
   const signature = crypto.createHmac('sha256', apiSecret)
   .update(queryString)
   .digest('hex');
@@ -51,7 +76,9 @@ export  const POST = async (req:any) => {
   console.log("and return:", signature);
 
   // Fetch binane order api with signature
-  try {
+
+
+    try {
     console.log(`Sending request to Binance API with URL: https://api.binance.com/api/v3/order/test?${queryString}&signature=${signature}`);
 
     let res = await fetchWithTimeout(`https://api.binance.com/api/v3/order/test?${queryString}&signature=${signature}`, {
@@ -83,6 +110,7 @@ export  const POST = async (req:any) => {
         return NextResponse.json({error: error.message});
       }
     }
+    
   
  
   
--- app/components/customLimitOrder/binance/CustomLimitOrder.tsx
+++ app/components/customLimitOrder/binance/CustomLimitOrder.tsx
@@ -1,11 +1,9 @@
 'use client'
-import React, { use, useState} from 'react';
-import { useEffect } from 'react';
-import { NextResponse } from 'next/server';
-import crypto from 'crypto';
+import React, { use, useState,useEffect} from 'react';
+import { useWebSocket }from '../../../hooks/useWebSocket';
 
 
-import { useRouter } from 'next/navigation'
+
 
 type Client = {
   apiKey: string;
@@ -18,10 +16,13 @@ type MyOrder = {
   side: string;
   type: string;
   quantity: number;
-  price: number;
+  price?: number;
   stopPrice: number;
   takeProfitPrice: number;
   balances: number;
+  icebergQty?: number;
+  timeInForce?: string; 
+
 };
 
 
@@ -31,50 +32,132 @@ const CustomLimitOrder =   () => {
   const [symbol, setSymbol] = useState('');
   const [side, setSide] = useState('BUY');
   const [type, setType] = useState('LIMIT');
-  const [quantity, setQuantity] = useState(0);
-  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState('');
+  const [price, setPrice] = useState('');
+  const [icebergQty, setIcebergQty] = useState('');
+  const [timeInForce, setTimeInForce] = useState<string | null>('GTC');
+  const [balances, setBalances] = useState('');
+  const [takeProfitPrice, setTakeProfitPrice] = useState('');
   // const [recvWindow, setRecvWindow] = useState('5000');
-  // const [stopPrice, setStopPrice] = useState('10500');
+   const [stopPrice, setStopPrice] = useState('');
+   const [debouncedSymbol, setDebouncedSymbol] = useState("");
+; // delays update by 500ms
 
-  const handleSubmit =  (e) => {
-    console.log(`Sending request to server with body:`, {
-      symbol: symbol,
-      side: side,
-      type: type,
-      quantity :quantity,
-      price: price,
-    });
-    e.preventDefault();
-    const response =  fetch(
-      '/api/binance/customOrder',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          symbol: symbol,
-          side: side,
-          type: type,
-          quantity :quantity,
-          price: price,
-        }),
-      })
-      .then((res) =>{ 
-        console.log('Response from server:', res);
-        res.json()
-      })
-      .then((data) => { console.log(data) })
-      .catch((err) => console.log(err));
+  const {data, error } = useWebSocket(debouncedSymbol)
 
-    return response;
+  useEffect(() => {
+    if (data) {
+        const parsedData = JSON.parse(data);
+        if (parsedData.eventType === "executionReport" && parsedData.orderStatus === "FILLED") {
+            console.log("Limit order filled, placing OCO order...");
 
+            // The body for your OCO order
+            let ocoOrder: OCOOrder = {
+                symbol: symbol,
+                side: side,
+                stopPrice: stopPrice === "" ? 0 : parseFloat(stopPrice),
+                takeProfitPrice: takeProfitPrice === "" ? 0 : parseFloat(takeProfitPrice),
+                quantity: quantity === "" ? 0 : parseFloat(quantity)
+            };
+
+            // The API request to place the OCO order
+            fetch(
+                '/api/binance/customOCOOrder',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(ocoOrder),
+                }
+            )
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+        }
+    }
+}, [data]);
+  useEffect(() => {
+    // set up a delay
+    const timerId = setTimeout(() => {
+      setDebouncedSymbol(symbol);
+    }, 1000);
   
+    // clean up function
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [symbol]);
+
+
+const handleOrderTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const newType = e.target.value;
+  setType(newType);
+
+  // If the new order type is "LIMIT", set timeInForce to "GTC", otherwise set it to null
+  if (newType === "LIMIT") {
+    setTimeInForce("GTC");
+  } else {
+    setTimeInForce(null);
+  }
+};
+
+const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+
+  let body: MyOrder = {
+    symbol: symbol,
+    side: side,
+    type: type,
+    quantity: quantity === "" ? 0 : parseFloat(quantity),
+    stopPrice: stopPrice === "" ? 0 : parseFloat(stopPrice),
+    takeProfitPrice: takeProfitPrice === "" ? 0 : parseFloat(takeProfitPrice),
+    balances: balances === "" ? 0 : parseFloat(balances)
   };
+
+  if (type === "LIMIT") {
+    body.price = parseFloat(price);
+
+    if (timeInForce) {
+      body.timeInForce = timeInForce;
+    }
+
+    if (icebergQty !== "") {
+      body.icebergQty = parseFloat(icebergQty);
+    }
+  }
+
+  console.log(`Sending request to server with body:`, body);
+
+  const response = fetch(
+    '/api/binance/customOrder',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body),
+    })
+    .then((res) => { 
+      console.log('Response from server:', res);
+      return res.json();
+    })
+    .then((data) => { 
+      console.log(data);
+      setSymbol(symbol);
+    })
+    .catch((err) => console.log(err));
+      
+  return response;
+};
+
 
   return (
     <form onSubmit={ handleSubmit}>
+      <div className="grid grid-rows-auto">
+
       <input
+        className='p-2'
         type="text"
         placeholder="Symbol"
         value={symbol}
@@ -82,6 +165,7 @@ const CustomLimitOrder =   () => {
           setSymbol(e.target.value)}}
       />
       <select
+        className='p-2'
         name="side"
         defaultValue="BUY"
         onChange={(e) => setSide(e.target.value)}
@@ -90,24 +174,45 @@ const CustomLimitOrder =   () => {
         <option value="SELL">Sell</option>
       </select>
       <select
+      className='p-2'
       name="type"
       defaultValue="LIMIT"
-      onChange={(e) => setType(e.target.value)}
+      onChange={handleOrderTypeChange}
       >
-        <option value="MARKET">Limit</option>
-        <option value="LIMIT">Market</option>
+       
+        <option value="LIMIT">LIMIT</option>
+        <option value="MARKET">MARKET</option>
       </select>
       <input
+        className='p-2'
         type="number"
         placeholder="Quantity"
         value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
+        onChange={(e) => setQuantity(e.target.value)}
       />
       <input
+        className='p-2'
         type="number"
         placeholder="Price"
         value={price}
-        onChange={(e) => setPrice(Number(e.target.value))}
+        onChange={(e) => setPrice(e.target.value)}
+        disabled={type === "MARKET"}
+
+      />
+      <input
+        className='p-2'
+      type='number'
+      placeholder='IcebergQty'
+      value={icebergQty}
+      onChange={(e) => setIcebergQty(e.target.value)}
+      disabled={type === "MARKET"}
+
+      />
+      <input className='p-2'
+      type='number'
+      placeholder='StopPrice'
+      value = {stopPrice} 
+      onChange = {(e) => setStopPrice(e.target.value)}
       />
       {/* <input
         type="number"
@@ -115,10 +220,16 @@ const CustomLimitOrder =   () => {
         value={stopPrice}
         onChange={(e) => setStopPrice(e.target.value)}
       /> */}
+      </div>
+      <div className="flex flex-col justify-center ">
+
       <button 
-      onClick={() =>console.log(' button clicked')}
+      className=' p-2 bg-red-300 text-center'
+      onClick={() => console.log(' button clicked')}
       type="submit"
    >Place Order</button>
+      </div>
+
     </form>
   );
 
